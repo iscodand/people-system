d920e2822d81369ba9d18398819cab2e05c03bfb
iscodand <iscodand@outlook.com>
2022-05-06 12:28:51 -0300

    formating text output

--- app/functions.py
+++ app/functions.py
@@ -17,14 +17,12 @@ def read_archive():
 
 def modify_archive():
 
-    global name, age
-
     print(line())
 
     try:
         with open(archive, 'a+') as open_archive:
-            name = open_archive.writelines(str(input('Nome: ')).upper() + f'{" ">10}')
-            age = open_archive.writelines(str(input('Idade: ')) + '\n')
+            name = open_archive.writelines(str(input('Nome: ')).upper() + f'{" ":>15}')
+            age = open_archive.writelines((str(input('Idade: ')) + f'{" ":<5}' + '\n'))
             
     except Exception as e:
         print(f'\033[1;31m---- {e} ----\033[m')
